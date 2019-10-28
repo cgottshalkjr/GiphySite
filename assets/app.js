14ad59d9780590a2a7e7ca2d8e1eb0e0710c96ec
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-10-27 20:32:57 -0400

    New buttons are pushing to screen when user inputs choice, and appearing in console log

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,33 @@
 var topics = ["Branded To Kill", "Seven Samurai", "Ikiru", "In The Mood For Love", "Taxi Driver", "Stanley Kubrick", "Akira Kurosawa", "Andrei Tarkovsky", "Rainer Werner Fassbinder", "No Country For Old Men", "Harry Dean Stanton", "Wim Wenders", "Ichi The Killer", "The Passion Of Joan Of Arc"];
 
-$("button").on("click", function () {
+function displayMovieInfo() {
 
-    var APIKEY = "Kjfr6NFOahJ1dHWskpCZ5XzxIKmXsDtC";
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + APIKEY + "&limit=10&rating="
+    var movieGIF = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movieGIF + "&apikey=Kjfr6NFOahJ1dHWskpCZ5XzxIKmXsDtC&limit=10&rating="
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        var movieDiv = $("<div class='movieGIF'>");
 
+        $("#film-gif-display").prepend(movieDiv);
+    });
+}
+
+//When a button is clicked...
+$("#add-movie-gif").on("click", function (event) {
+    event.preventDefault();
+    var movie = $("#movie-input").val().trim();
+    topics.push(movie);
+    createButtons();
 });
 
+
 function createButtons() {
+    $("#buttons-view").empty();
     for (var i = 0; i < topics.length; i++) {
-        $("#buttons-view").empty
         var movieButton = $("<button>");
         movieButton.addClass("movie btn btn-warning m-2 p-2");
         movieButton.attr("data-name", topics[i]);
@@ -19,4 +36,21 @@ function createButtons() {
     }
 }
 
+$(document).on("click", ".movie", displayMovieInfo);
+
 createButtons();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
